ff12400dcd2531c3af58d51434bef9393dfddbdb
안민현 <alsgus2ek@naver.com>
2021-04-22 16:14:41 +0900

    Initial commit

---
+++ src/main/java/com/example/todo/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.example.todo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+
+        http
+                .formLogin()
+                .loginPage("/login")
+                .usernameParameter("userId")
+                .usernameParameter("userPw")
+                .loginProcessingUrl("/login");
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
