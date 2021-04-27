28d9620e52e05610abc85e2b09512715ad7614c3
안민현 <alsgus2ek@naver.com>
2021-04-27 15:10:15 +0900

    Todo Repository Add FindAllByMember

--- src/main/java/com/example/todo/repository/ProfileRepository.java
+++ src/main/java/com/example/todo/repository/ProfileRepository.java
@@ -4,6 +4,10 @@ import com.example.todo.entity.profile.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
+
+    Optional<Profile> findByMember(String memberId);
 }
